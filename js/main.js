3a5e76b5e39b6f9fbd569c9ebb2b60470fcc310c
sureshfizzy <61338311+sureshfizzy@users.noreply.github.com>
2020-09-28 15:25:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,6 +25,17 @@ jQuery(document).ready(function(){
       }
     
      });
+    $(window).scroll(function(){
+        var top=$(window).scrollTop();
+        if(top>=60){
+        $("header").addClass('secondary');
+    }
+    else
+         if($("header").hasClass('secondary')){
+             $("header").removeClass('secondary');
+     }
+        
+    });
                        
                                       
 });
